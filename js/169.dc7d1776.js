22f4451884ce54da174c7ab0716de0caecae2215
Imran <umurzakov117ali@gmail.com>
2022-08-20 15:40:23 +0500

    Updates

---
+++ js/169.dc7d1776.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkaler_aqua"]=self["webpackChunkaler_aqua"]||[]).push([[169],{1169:function(a,u,e){e.r(u),e.d(u,{default:function(){return c}});var n=e(3396);function r(a,u,e,r,t,l){return(0,n.wg)(),(0,n.iD)("div",null," Quality pages ")}var t={name:"Quality-vue"},l=e(89);const i=(0,l.Z)(t,[["render",r]]);var c=i}}]);
+//# sourceMappingURL=169.dc7d1776.js.map
---
+++ js/221.b5035ac2.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkaler_aqua"]=self["webpackChunkaler_aqua"]||[]).push([[221],{4221:function(e,r,n){function a(e,r,n,a,u,f){return" sdfsdfdg "}n.r(r),n.d(r,{default:function(){return c}});var u={name:"Grocery-feed"},f=n(89);const t=(0,f.Z)(u,[["render",a]]);var c=t}}]);
+//# sourceMappingURL=221.b5035ac2.js.map
